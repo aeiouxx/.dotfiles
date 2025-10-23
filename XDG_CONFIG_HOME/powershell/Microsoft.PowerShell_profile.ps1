e7b17ce1dda9f2a1b7c6a0778c840fe6e01f4af8
aeiouxx <aeiouxx.dev@gmail.com>
2025-10-23 23:50:24 +0200

    Add lsd

--- XDG_CONFIG_HOME/powershell/Microsoft.PowerShell_profile.ps1
+++ XDG_CONFIG_HOME/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,15 +6,17 @@ Remove-Item Alias:find -ErrorAction SilentlyContinue
 Set-Alias vi nvim
 Set-PSReadLineOption -Colors @{ InlinePrediction = "$([char]0x1b)[90m" }
 
-# Stuff
-$proj = "D:/Projects"
+function ls { lsd -la @Args }
 
 # Keybindings
+# TODO: Figure out non colliding bindings with glaze + wezterm
 Set-PSReadLineKeyHandler -Key Ctrl+a -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Key Ctrl+e -Function EndOfLine
 Set-PSReadLineKeyHandler -Key Ctrl+k -Function KillLine
 Set-PSReadLineKeyHandler -Key Ctrl+j -Function AcceptSuggestion
 
+
+Import-Module posh-git
 # Oh My Posh
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
-Import-Module posh-git
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
